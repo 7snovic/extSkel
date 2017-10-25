a4d54207f375ee1f4261765885a72f346682c80c
hassan <hah.alex@yahoo.com>
2017-10-25 20:53:28 +0200

    add ext_class example

--- examples/ext_class.php
+++ examples/ext_class.php
@@ -5,7 +5,21 @@ class extClass
 {
     public $protoType = 'class';
 
+    public $namespace = 'MyAwesomeExt';
+    
+    public $className = 'HelloWorld';
+
     public function helloWorld()
+    {
+
+    }
+
+    public function functionWithParam(string $param)
+    {
+
+    }
+
+    public function functionWithOptionalParams(string $param, $count = '')
     {
 
     }
